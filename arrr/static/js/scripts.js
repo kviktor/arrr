cdd694282c3a1fb3d63110a443904d17b6935f1c
kviktor <kviktor@cloud.bme.hu>
2015-11-21 19:23:50 +0100

    arrr: basic design

--- arrr/static/js/scripts.js
+++ arrr/static/js/scripts.js
@@ -1,1 +1,27 @@
 /* Project specific Javascript goes here. */
+
+function sideNav() {
+
+  if ($(window).width() < 769) {
+
+    $('.off-canvas-wrap').removeClass('move-right');
+
+    $('.left-off-canvas-toggle').show();
+
+  } else {
+
+    $('.off-canvas-wrap').addClass('move-right');
+
+    $('.left-off-canvas-toggle').hide();
+
+  }  
+
+}
+
+
+
+$(window).resize(function() {
+
+  sideNav();
+
+});
